327ff89f8ad84675766dabb01f63337ab7451536
ContactJHC <95849631+ContactJHC@users.noreply.github.com>
2023-07-31 13:42:35 +0200

    Intro update

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -15,7 +15,7 @@ export default function Intro() {
         My passions lie in optimizing efficiency and integrating these principles into everyday life. As for my values, I prioritize teamwork, having fun, building trust, and encouraging innovation. I'm eager to work with empathetic, forward-thinking natural leaders who are shaping the future. I'm only interested in collaborating with companies that prioritize people and innovation.
         </p>
         <p className='text-sm max-w-xl mb-6 font-bold'>
-        To sum up, I'm a recent CS graduate and experienced front-end developer with a focus on JavaScript React mobile-first development. I'm excited to join an interactive, hardworking team where I can continue to expand my knowledge of JavaScript, perfecting accessibility, performance, and responsive design.
+        To sum up, I'm a recent CS graduate and experienced front-end developer with a focus on JavaScript React mobile-first development. I'm excited to join an interactive, hardworking team where I can continue to expand my knowledge of JavaScript, perfecting accessibility, performance, and responsive design. Below are my three favorite apps out of the thirteen I created and I'm looking forward to showing to you the remaining 11 left ! Until then, you can already find their summaries on my resume which I sent to you.
         </p>
     </div>
   )
